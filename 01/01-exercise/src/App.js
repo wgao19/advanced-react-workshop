1a4f44302ba41fb5bce0efe929ed0091188dffa4
Wei Gao <wgao19@gmail.com>
2018-08-15 11:18:18 -0600

    Implement exercise 01

--- 01/01-exercise/src/App.js
+++ 01/01-exercise/src/App.js
@@ -4,6 +4,30 @@ import subscribeToMessages from "./messages";
 import FadeIn from "./FadeIn";
 
 class PinScrollToBottom extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getSnapshotBeforeUpdate() {
+    const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+    return scrollHeight === scrollTop + clientHeight;
+  }
+
+  componentDidMount() {
+    this.scroll();
+  }
+
+  componentDidUpdate(prevProps, prevState, atBottom) {
+    atBottom && this.scroll();
+  }
+
+  scroll() {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth"
+    });
+  }
+
   render() {
     return this.props.children;
   }
